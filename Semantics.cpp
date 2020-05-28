4cc3015d56adfde13176c7b7aa64b98995c055dd
Ofree <ofree21@gmail.com>
2020-05-28 09:22:52 +0300

    /

--- Semantics.cpp
+++ Semantics.cpp
@@ -935,10 +935,10 @@ Node* semantics27(Node *id, Node *lparen, Node *explist, Node *rparen, DataStruc
     //Expression* e = dynamic_cast<Expression*>(explist);
    // cout<<"dynamic cast"<<endl;
     list<string>* types = explist->getTypes();
-    cout<<"ofree: "<< *types->begin()<<endl;
+    //cout<<"ofree: "<< *types->begin()<<endl;
     list<Symbol*>* s = getFunctionsArgs(id, tables);
     Symbol* symbol = *s->begin();
-    cout<<"ofree: "<< symbol->getType()<<endl;
+    //cout<<"ofree: "<< symbol->getType()<<endl;
 
     // cout<<"create s"<<endl;
 
